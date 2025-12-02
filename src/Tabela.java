710e2be94ee72f0f0fe360059749a17e80403929
Invencible-Maximilian <maximilianwachter@outlook.com>
2025-12-01 23:56:26 -0300

    Commit da Tabela

---
+++ src/Tabela.java
@@ -0,0 +1,10 @@
+import java.lang.annotation.*;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.FIELD, ElementType.METHOD, ElementType.CONSTRUCTOR})
+
+public @interface Tabela {
+
+    String NomeTabela() ;
+}
