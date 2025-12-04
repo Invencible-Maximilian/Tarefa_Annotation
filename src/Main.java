54d679d04ac51b12961e107c453459897160ad77
Invencible-Maximilian <maximilianwachter@outlook.com>
2025-12-04 01:37:29 -0300

    Adicionando reflection

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+
+        // Obtém a classe
+        Class<?> clazz = ClasseComTabela.class;
+
+        // Verifica se a classe possui a annotation Tabela
+        if (clazz.isAnnotationPresent(Tabela.class)) {
+
+            // Recupera a annotation
+            Tabela tabela = clazz.getAnnotation(Tabela.class);
+
+            // Lê o valor da propriedade da annotation
+            String nomeTabela = tabela.NomeTabela();
+
+            System.out.println("Nome da tabela: " + nomeTabela);
+        } else {
+            System.out.println("A classe não possui a annotation @Tabela.");
+        }
+    }
 }
